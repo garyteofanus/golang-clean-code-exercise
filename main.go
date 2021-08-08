299cfe0bfbcdc059b2b7a664540ca2d866c9055b
garyteofanus <garyteofanus@gmail.com>
2021-08-08 09:48:39 +0700

    Refactor long method

--- main.go
+++ main.go
@@ -1,46 +1,36 @@
 package main
 
 import (
-        "fmt"
+	"fmt"
 
-        "github.com/hezbymuhammad/golang-clean-code-exercise/models"
+	"github.com/hezbymuhammad/golang-clean-code-exercise/models"
 )
 
 func main() {
-        salesData := []int{1000, 22000, 33000, 44000, 55000, 66000, 77000}
-        expensesData := []int{15000, 25000, 35000, 45000, 55000, 65000, 75000}
+	salesData := []int{1000, 22000, 33000, 44000, 55000, 66000, 77000}
+	expensesData := []int{15000, 25000, 35000, 45000, 55000, 65000, 75000}
 
-        s := models.NewSales(salesData)
-        e := models.NewExpenses(expensesData)
+	s := models.NewSales(salesData)
+	e := models.NewExpenses(expensesData)
 
-        salesTotal := 0
-        expensesTotal := 0
-        salesFirstTwoDays := 0
-        expensesFirstTwoDays := 0
+	salesTotal := 0
+	expensesTotal := 0
+	salesFirstTwoDays := 0
+	expensesFirstTwoDays := 0
 
-        // TODO: create separate method for calculating sales and expense
-        // i.e. salesTotal := s.GetTotal()
-        // i.e. expensesTotal := e.GetTotal()
-        for i := 0; i <= 6; i++ {
-                salesTotal += s.Get(i)
-                expensesTotal += e.Get(i)
-        }
+	salesTotal = s.GetTotal()
+	expensesTotal = e.GetTotal()
 
-        // TODO: create separate method for calculating sales and expense
-        // i.e. salesFirstTwoDays := s.GetTotalWithinRange(0, 2)
-        // i.e. expensesFirstTwoDays := e.GetTotalWithinRange(0, 2)
-        for i := 0; i <= 1; i++ {
-                salesFirstTwoDays += s.Get(i)
-                expensesFirstTwoDays += e.Get(i)
-        }
+	salesFirstTwoDays = s.GetTotalWithinRange(0, 2)
+	expensesFirstTwoDays = e.GetTotalWithinRange(0, 2)
 
-        fmt.Println("=======##=======")
-        fmt.Printf("Sales in the first 2 days is: %v\n", salesFirstTwoDays)
-        fmt.Printf("Expenses in the first 2 days is: %v\n", expensesFirstTwoDays)
-        fmt.Printf("Total profit in the first 2 days is: %v\n", salesFirstTwoDays - expensesFirstTwoDays)
-        fmt.Println("")
-        fmt.Printf("Sales total is: %v\n", salesTotal)
-        fmt.Printf("Expenses total is: %v\n", expensesTotal)
-        fmt.Printf("Total profit is: %v\n", salesTotal - expensesTotal)
-        fmt.Println("=======##=======")
+	fmt.Println("=======##=======")
+	fmt.Printf("Sales in the first 2 days is: %v\n", salesFirstTwoDays)
+	fmt.Printf("Expenses in the first 2 days is: %v\n", expensesFirstTwoDays)
+	fmt.Printf("Total profit in the first 2 days is: %v\n", salesFirstTwoDays-expensesFirstTwoDays)
+	fmt.Println("")
+	fmt.Printf("Sales total is: %v\n", salesTotal)
+	fmt.Printf("Expenses total is: %v\n", expensesTotal)
+	fmt.Printf("Total profit is: %v\n", salesTotal-expensesTotal)
+	fmt.Println("=======##=======")
 }
--- models/transactions.go
+++ models/transactions.go
@@ -1,35 +1,43 @@
 package models
 
 type transactions struct {
-        data map[int]int
+	data map[int]int
 }
 
-// TODO: implement this
 // i.e. go test is green
 func NewTransactions(d []int) Transactions {
-        data := make(map[int]int)
+	data := make(map[int]int)
 
-        for i, v := range d {
-                data[i] = v
-        }
+	for i, v := range d {
+		data[i] = v
+	}
 
-        return &transactions{data: data}
+	return &transactions{data: data}
 }
 
-// TODO: implement this
 // i.e. go test is green
 func (t *transactions) Get(idx int) int {
-        return t.data[idx]
+	return t.data[idx]
 }
 
-// TODO: implement this method
 // i.e. go test is green
 func (t *transactions) GetTotal() int {
-        return 0
+	ret := 0
+
+	for i := 0; i <= 6; i++ {
+		ret += t.Get(i)
+	}
+
+	return ret
 }
 
-// TODO: implement this method
 // i.e. go test is green
 func (t *transactions) GetTotalWithinRange(i, j int) int {
-        return 0
+	ret := 0
+
+	for i := i; i <= j; i++ {
+		ret += t.Get(i)
+	}
+
+	return ret
 }
