23bd8ca0223fc6597fafa7e76cc4521838c8c7d3
garyteofanus <garyteofanus@gmail.com>
2021-08-08 09:25:26 +0700

    Remove duplicate code

--- main.go
+++ main.go
@@ -1,40 +1,45 @@
 package main
 
 import (
-        "fmt"
+	"fmt"
 
-        "github.com/hezbymuhammad/golang-clean-code-exercise/models"
+	"github.com/hezbymuhammad/golang-clean-code-exercise/models"
 )
 
 func main() {
-        salesData := []int{1000, 22000, 33000, 44000, 55000, 66000, 77000}
-        expensesData := []int{15000, 25000, 35000, 45000, 55000, 65000, 75000}
+	salesData := []int{1000, 22000, 33000, 44000, 55000, 66000, 77000}
+	expensesData := []int{15000, 25000, 35000, 45000, 55000, 65000, 75000}
 
-        s := models.NewSales(salesData)
-        e := models.NewExpenses(expensesData)
+	//s := models.NewSales(salesData)
+	//e := models.NewExpenses(expensesData)
 
-        salesTotal := 0
-        expensesTotal := 0
-        salesFirstTwoDays := 0
-        expensesFirstTwoDays := 0
+	s := models.NewTransactions(salesData)
+	e := models.NewTransactions(expensesData)
 
-        for i := 0; i <= 6; i++ {
-                salesTotal += s.Get(i)
-                expensesTotal += e.Get(i)
-        }
+	fmt.Println(s, e)
 
-        for i := 0; i <= 1; i++ {
-                salesFirstTwoDays += s.Get(i)
-                expensesFirstTwoDays += e.Get(i)
-        }
+	salesTotal := 0
+	expensesTotal := 0
+	salesFirstTwoDays := 0
+	expensesFirstTwoDays := 0
 
-        fmt.Println("=======##=======")
-        fmt.Printf("Sales in the first 2 days is: %v\n", salesFirstTwoDays)
-        fmt.Printf("Expenses in the first 2 days is: %v\n", expensesFirstTwoDays)
-        fmt.Printf("Total profit in the first 2 days is: %v\n", salesFirstTwoDays - expensesFirstTwoDays)
-        fmt.Println("")
-        fmt.Printf("Sales total is: %v\n", salesTotal)
-        fmt.Printf("Expenses total is: %v\n", expensesTotal)
-        fmt.Printf("Total profit is: %v\n", salesTotal - expensesTotal)
-        fmt.Println("=======##=======")
+	for i := 0; i <= 6; i++ {
+		salesTotal += s.Get(i)
+		expensesTotal += e.Get(i)
+	}
+
+	for i := 0; i <= 1; i++ {
+		salesFirstTwoDays += s.Get(i)
+		expensesFirstTwoDays += e.Get(i)
+	}
+
+	fmt.Println("=======##=======")
+	fmt.Printf("Sales in the first 2 days is: %v\n", salesFirstTwoDays)
+	fmt.Printf("Expenses in the first 2 days is: %v\n", expensesFirstTwoDays)
+	fmt.Printf("Total profit in the first 2 days is: %v\n", salesFirstTwoDays-expensesFirstTwoDays)
+	fmt.Println("")
+	fmt.Printf("Sales total is: %v\n", salesTotal)
+	fmt.Printf("Expenses total is: %v\n", expensesTotal)
+	fmt.Printf("Total profit is: %v\n", salesTotal-expensesTotal)
+	fmt.Println("=======##=======")
 }
--- models/transactions.go
+++ models/transactions.go
@@ -1,19 +1,23 @@
 package models
 
 type transactions struct {
-        data map[int]int
+	data map[int]int
 }
 
 // TODO: implement this
 // i.e. go test is green
 func NewTransactions(d []int) Transactions {
-        data := make(map[int]int)
+	data := make(map[int]int)
 
-        return &transactions{data: data}
+	for i, v := range d {
+		data[i] = v
+	}
+
+	return &transactions{data: data}
 }
 
 // TODO: implement this
 // i.e. go test is green
 func (t *transactions) Get(idx int) int {
-        return 0
+	return t.data[idx]
 }
